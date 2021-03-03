f41066a822b90539a2173d1cf4b07dc0637a4ac4
Poncho <jplyles1212@gmail.com>
2021-03-03 08:13:01 -0800

    Create platforms for game

--- PlatformGame/PlatformGame/Form1.Designer.cs
+++ PlatformGame/PlatformGame/Form1.Designer.cs
@@ -30,27 +30,40 @@ namespace PlatformGame
         private void InitializeComponent()
         {
             this.txtScore = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtScore
             // 
             this.txtScore.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtScore.Location = new System.Drawing.Point(492, 9);
+            this.txtScore.Location = new System.Drawing.Point(493, 10);
             this.txtScore.Name = "txtScore";
             this.txtScore.Size = new System.Drawing.Size(286, 88);
             this.txtScore.TabIndex = 0;
             this.txtScore.Text = "Score: 0";
             this.txtScore.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 858);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 36);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(651, 753);
+            this.ClientSize = new System.Drawing.Size(780, 900);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtScore);
             this.Name = "Form1";
             this.Text = "Platform Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +71,7 @@ namespace PlatformGame
         #endregion
 
         private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- PlatformGame/PlatformGame/Form1.cs
+++ PlatformGame/PlatformGame/Form1.cs
@@ -16,5 +16,10 @@ namespace PlatformGame
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
