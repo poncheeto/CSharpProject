c129e4f17b6a013bb3dd91272cf0eed1ef3058ea
Poncho <jplyles1212@gmail.com>
2021-03-09 07:44:32 -0800

    Remove grid creation code from Game class and add to World class

--- AdventureMaze/AdventureMaze/Game.cs
+++ AdventureMaze/AdventureMaze/Game.cs
@@ -16,20 +16,8 @@ namespace AdventureMaze
                 {"4", "5", "6" },
                 {"7", "8", "9" },
             };
-
-            // Prints out grid to the console
-            int rows = grid.GetLength(0);
-            int columns = grid.GetLength(1);
-
-            for (int y = 0; y < rows; y++)
-            {
-                for (int x = 0; x < columns; x++)
-                {
-                    string element = grid[y, x];
-                    SetCursorPosition(x, y);
-                    Write(element);
-                }
-            }
+            World myWorld = new World(grid);
+            myWorld.Draw();
 
             WriteLine("\n\nPress any key to exit...");
             ReadKey(true);
--- AdventureMaze/AdventureMaze/World.cs
+++ AdventureMaze/AdventureMaze/World.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Console;
 
 namespace AdventureMaze
 {
     class World
     {
+        private string[,] Grid;
+        private int Rows;
+        private int Columns;
+
+        public World(string[,] grid)
+        {
+            Grid = grid;
+            Rows = Grid.GetLength(0);
+            Columns = Grid.GetLength(1);
+        }
+
+        // Prints out grid to the console
+        public void Draw()
+        {
+            for (int y = 0; y < Rows; y++)
+            {
+                for (int x = 0; x < Columns; x++)
+                {
+                    string element = Grid[y, x];
+                    SetCursorPosition(x, y);
+                    Write(element);
+                }
+            }
+        }
     }
 }
