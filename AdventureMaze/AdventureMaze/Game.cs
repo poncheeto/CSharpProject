30495b924db03bfad4d3c68d6ecce74512462d66
Poncho <jplyles1212@gmail.com>
2021-03-09 07:24:37 -0800

    Create sample grid to test printing grid to console

--- AdventureMaze/AdventureMaze/Game.cs
+++ AdventureMaze/AdventureMaze/Game.cs
@@ -9,10 +9,27 @@ namespace AdventureMaze
     {
         public void Start()
         {
-            WriteLine("Game is starting");
+            // Assign a maze map to grid
+            string[,] grid =
+            {
+                {"1", "2", "3" },
+                {"4", "5", "6" },
+                {"7", "8", "9" },
+            };
 
-            SetCursorPosition(4, 2);
-            Write("X");
+            // Prints out grid to the console
+            int rows = grid.GetLength(0);
+            int columns = grid.GetLength(1);
+
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < columns; x++)
+                {
+                    string element = grid[y, x];
+                    SetCursorPosition(x, y);
+                    Write(element);
+                }
+            }
 
             WriteLine("\n\nPress any key to exit...");
             ReadKey(true);
