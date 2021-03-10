6f45155efbc5602d7e7912320286c765c33ccc55
Poncho <jplyles1212@gmail.com>
2021-03-09 19:14:25 -0800

    Add Draw method to player class to draw itself to console depending on location

--- AdventureMaze/AdventureMaze/Player.cs
+++ AdventureMaze/AdventureMaze/Player.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Console;
 
 namespace AdventureMaze
 {
     class Player
     {
+        
+        public int X { get; set; }
+        public int Y { get; set; }
+        private string PlayerMarker;
+        private ConsoleColor PlayerColor;
+
+        // Initialize initial position on player construction
+        public Player(int initialX, int initialY)
+        {
+            X = initialX;
+            Y = initialY;
+            PlayerMarker = "O";
+            PlayerColor = ConsoleColor.DarkRed;
+        }
+
+
+        // Draw player
+        public void Draw()
+        {
+            ForegroundColor = PlayerColor;
+            SetCursorPosition(X, Y);
+            Write(PlayerMarker);
+            ResetColor();
+
+        }
     }
 }
